23230e4b5daa6a71e3c58b92fd58a9c39898a636
Emma Henriksson <emmaha.eh@gmail.com>
2024-05-21 13:10:56 +0000

    Create get_names_data function to get user input

--- run.py
+++ run.py
@@ -7,15 +7,21 @@ SCOPE = [
     "https://www.googleapis.com/auth/drive"
     ]
 
-CREDS = Credentials.from_service_account_file('cred.json')
+CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('top3_popular_names')
 
-boynames = SHEET.worksheet('boynames')
-databoy = boynames.get_all_values()
-print(databoy)
+def get_names_data():
+    """
+    Get name input from the user.
+    """
+    print("Please enter the name you would like to know more about")
+    print("The names are: Noah, Hugo, William, Elsa, Vera, Alma.")
+    print("One name at a time.\n")
 
-girlnames = SHEET.worksheet('girlnames')
-datagirl = girlnames.get_all_values()
-print(datagirl)
+    data_str = input("Enter the name you have chosen here: ")
+    print(f"The data provided is {data_str}")
+
+
+get_names_data()
