eefbf694fe062a92d1980285ea79e40c337de2fd
Emma Henriksson <emmaha.eh@gmail.com>
2024-05-22 08:15:04 +0000

    Add data from worksheet

--- run.py
+++ run.py
@@ -18,38 +18,53 @@ def get_names_data(names_list):
     Args:
     - names_list: List of names to choose from.
     """
-def get_names_data(names_list):
-    """
-    Get name input from the user and provide associated data if available.
-    Args:
-    - names_list: List of names to choose from.
-    """
-    while True:  # Start an infinite loop
+    while True:  
         print("Please enter the name you would like to know more about.")
         print("The available names are:", ", ".join(names_list) + ".")
         print("Enter one name at a time.\n")
 
-        data_str = input("Enter the name you have chosen here: ")
+        data_str = input("Enter the name you have chosen here, if you would like to stop please enter Stop: ")
 
         if data_str in names_list:
             print(f"Data available for {data_str}: ")
             if data_str == "Noah": 
-                print("from worksheetNoah")
+                worksheet = SHEET.worksheet("boynames")
+                column_data = worksheet.col_values(1)
+                print("First you'll see the name, then the amount of children being born in the last 3 years", column_data)
+                    
             elif data_str == "Hugo":
-                print("from worksheetHugo")
+                worksheet = SHEET.worksheet("boynames")
+                column_data = worksheet.col_values(2)
+                print("First you'll see the name, then the amount of children being born in the last 3 years", column_data)
+            
             elif data_str == "William":
-                print("from worksheetWilliam")
+                worksheet = SHEET.worksheet("boynames")
+                column_data = worksheet.col_values(3)
+                print("First you'll see the name, then the amount of children being born in the last 3 years", column_data)
+            
             elif data_str == "Elsa":
-                print("from worksheetElsa")
+                worksheet = SHEET.worksheet("girlnames")
+                column_data = worksheet.col_values(1)
+                print("First you'll see the name, then the amount of children being born in the last 3 years", column_data)
+            
             elif data_str == "Vera":
-                print("from worksheetVera")
+                worksheet = SHEET.worksheet("girlnames")
+                column_data = worksheet.col_values(2)
+                print("First you'll see the name, then the amount of children being born in the last 3 years", column_data)
+            
             elif data_str == "Alma":
-                print("from worksheetAlma")
-            break 
+                worksheet = SHEET.worksheet("girlnames")
+                column_data = worksheet.col_values(3)
+                print("First you'll see the name, then the amount of children being born in the last 3 years", column_data)
+            
+        elif data_str == "Stop":
+            print("Thank you for using this survey, bye bye!")
+            break  
+                
         else:
             print("Invalid name. Please choose a name from the provided list.\n")
 
-
 if __name__ == "__main__":
     names_list = ["Noah", "Hugo", "William", "Elsa", "Vera", "Alma"]  
-    get_names_data(names_list) 
+    get_names_data(names_list)
+
